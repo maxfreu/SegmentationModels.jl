1ef70e0108466c22d2cb2c6c9e47ad7868993bd1
Documenter.jl <documenter@juliadocs.github.io>
2021-08-08 15:22:46 +0000

    build based on b6ec155

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SegmentationModels","category":"page"},{"location":"#SegmentationModels","page":"Home","title":"SegmentationModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SegmentationModels]","category":"page"}]
+}
